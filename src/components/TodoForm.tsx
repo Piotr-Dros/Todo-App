7bf0ee275b3a7a9dda4f32a2c9b44eca77502c37
Piotr Dros <piotr.dros01@gmail.com>
2023-05-14 19:45:16 +0200

    Small fix

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -23,7 +23,7 @@ const TodoForm = () => {
         <Checkbox isChecked={false} onChange={() => null} />
         <input
           type="text"
-          className="placholder:text-blue-placeholder bg-transparent text-sm caret-primary"
+          className="w-full placholder:text-blue-placeholder bg-transparent text-sm caret-primary"
           placeholder="Create a new todo..."
           value={todoInput}
           onChange={handleChange}
